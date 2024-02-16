1a25ff85c4fc8197e89f8d4f9fd8d29c69c58738
Alejandra <alejandravillalva2@gmail.com>
2024-02-16 10:52:57 +0100

    feat(testagendacontactosdao.java): configurando TestAgendaContactosDao

--- src/modelo/dao/AgendaContacto.java
+++ src/modelo/dao/AgendaContacto.java
@@ -1,2 +1,34 @@
-package modelo.dao;public class AgendaContacto {
+package modelo.dao;
+
+import java.util.ArrayList;
+
+import modelo.javabean.Contacto;
+
+public class AgendaContacto {
+    private String nombreAgenda;
+    private ArrayList<Contacto> contactos;
+    public AgendaContacto(String nombreAgenda, ArrayList<Contacto> contactos) {
+        super();
+        this.nombreAgenda = nombreAgenda;
+        this.contactos = contactos;
+    }
+    public AgendaContacto() {
+        super();
+    }
+    public String getNombreAgenda() {
+        return nombreAgenda;
+    }
+    public void setNombreAgenda(String nombreAgenda) {
+        this.nombreAgenda = nombreAgenda;
+    }
+    public ArrayList<Contacto> getContactos() {
+        return contactos;
+    }
+    public void setContactos(ArrayList<Contacto> contactos) {
+        this.contactos = contactos;
+    }
+    @Override
+    public String toString() {
+        return "AgendaContacto [nombreAgenda=" + nombreAgenda + ", contactos=" + contactos + "]";
+    }
 }
