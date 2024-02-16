33246bee3beb69466e1b56b5095cf22519d0792d
Alejandra <alejandravillalva2@gmail.com>
2024-02-16 10:11:21 +0100

    docs(agendacontactodao.java): documentando AgendaContactoDao

--- src/modelo/dao/AgendaContactoDao.java
+++ src/modelo/dao/AgendaContactoDao.java
@@ -1,2 +1,18 @@
-package modelo.dao;public interface AgendaContactoDao {
+package modelo.dao;
+
+import modelo.javabean.Contacto;
+
+import java.util.ArrayList;
+
+public interface AgendaContactoDao {
+
+    Boolean altaContacto(Contacto contacto);
+    Boolean eliminarContacto(Contacto contacto);
+    Contacto buscarUno(String nombre);
+    Contacto buscarTelefono(String telefono);
+    Contacto buscarEmail(String email);
+    ArrayList<Contacto> buscarContactosPorTresPrimeros(String nombre);
+    Boolean cambiarDatos(Contacto contacto);
+    ArrayList<Contacto> contactosPorEmpresa(String empresa);
+
 }
